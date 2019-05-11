5e5ff3d29d78a3a07f51bf23e0f55b04e45b0d18
tishksenia <tishksenia@yandex.ru>
2019-05-11 10:37:43 +0300

    gulpfile.js edit: added livereload

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,30 @@ const { src, dest, parallel, start, watch, series } = require('gulp');
 const sass = require('gulp-sass')
 const pug = require('gulp-pug');
 const concat = require('gulp-concat');
+const livereload = require('gulp-livereload');
 
 function css() {
   return src('src/scss/**')
   .pipe(sass())
   //.pipe(concat('style.css'))
   .pipe(dest('dist/css'))
+  .pipe(livereload())
 }
 
 function html() {
   return src('src/pug/*.pug')
     .pipe(pug())
     .pipe(dest('dist/html'))
+    .pipe(livereload())
 }
 
 function watch_css() {
-  return watch('src/scss/*.scss', series('css'));
+    livereload.listen();
+    return watch('src/scss/*.scss', series('css'));
 }
 function watch_html() {
-  return watch('src/pug/**/*.pug', series('html'));
+    livereload.listen();
+    return watch('src/pug/**/*.pug', series('html'));
 }
 exports.css = css;
 exports.html = html;
