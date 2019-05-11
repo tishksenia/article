e567ab1071a454c413f7d7a61fc5f8eefab80929
tishksenia <tishksenia@yandex.ru>
2019-05-11 19:52:01 +0300

    gulpfile.js update to work with js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const sass = require('gulp-sass')
 const pug = require('gulp-pug');
 const concat = require('gulp-concat');
 const livereload = require('gulp-livereload');
+const uglify = require('gulp-uglify');
+const jshint = require('gulp-jshint');
 
 function css() {
   return src('src/scss/**')
@@ -19,6 +21,16 @@ function html() {
     .pipe(livereload())
 }
 
+function js() {
+  return src('src/js/*.js')
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'))
+  .pipe(uglify())
+  .pipe(concat('app.js'))
+  .pipe(dest('dist/js/'))
+  //.pipe(livereload())
+}
+
 function watch_css() {
     livereload.listen();
     return watch('src/scss/*.scss', series('css'));
@@ -29,5 +41,6 @@ function watch_html() {
 }
 exports.css = css;
 exports.html = html;
+exports.js = js;
 exports.default = parallel(html, css);
 exports.watch = parallel(watch_css, watch_html);
