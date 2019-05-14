6a564cdf3ea685505f76c8ca197976c0198e8184
tishksenia <tishksenia@yandex.ru>
2019-05-14 12:36:05 +0300

    markup and event handler for the search field

---
+++ src/js/search-form.js
@@ -0,0 +1,28 @@
+var searchFormModule = (function() {
+    var toggle_button = document.querySelector('.menu button.icon-search1.glyph');
+    var search_form = document.querySelector('.header__search-form');
+    var header_link = document.querySelector('.menu a.header-link');
+
+	return {
+		addEventListeners: function() {
+            toggle_button.onclick = function() {
+                if(header_link.style.display != "none") {
+                    header_link.style.display = "none";
+                    search_form.style.display = "flex";
+                    toggle_button.classList.add('active');
+                }
+                else {
+                    header_link.style.display = "inline";
+                    search_form.style.display = "none";
+                    toggle_button.classList.remove('active');
+                }
+                
+            }
+		},
+		init: function() {
+			this.addEventListeners();
+		}
+	};
+})();
+
+searchFormModule.init();
